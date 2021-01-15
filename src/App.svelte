70b5b7fabae03057c9b41d927e3eefa021d0060c
Alexander Zenov <alexander.zenov@gmail.com>
2021-01-15 22:07:48 +0300

    Rename project

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import BaseLayout from "~components/BaseLayout.svelte";
-  import Container from "~components/Container.svelte";
-  import Quote from "~components/Quote/Quote.svelte";
+  import BaseLayout from '~components/BaseLayout.svelte';
+  import Container from '~components/Container.svelte';
+  import Quote from '~components/Quote/Quote.svelte';
 </script>
 
 <BaseLayout>
