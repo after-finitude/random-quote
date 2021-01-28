3ae68528df6a000eb71b1e0069faa30faac2459a
Alexander Zenov <alexander.zenov@gmail.com>
2021-01-28 22:53:56 +0300

    Add autocomplete and update deps

---
+++ src/components/Autocomplete.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Select from 'svelte-select';
+
+  export let items: Record<string, any>[];
+  export let placeholder: string;
+  export let showChevron: boolean;
+  export let onSelect: (e: any) => void;
+</script>
+
+<div class="container">
+  <Select {items} {showChevron} {placeholder} on:select={onSelect} />
+</div>
+
+<style lang="scss">
+  .container {
+    --border: 2px solid #512da8;
+    --borderHoverColor: #512da8;
+    --borderFocusColor: #512da8;
+    --itemHoverBG: rgba(81, 45, 168, 0.4);
+    --itemIsActiveBG: #512da8;
+    --inputFontSize: 18px;
+
+    height: 48px;
+    width: 560px;
+  }
+</style>
--- src/pages/Book.svelte
+++ src/pages/Book.svelte
@@ -1,7 +1,33 @@
-<script>
+<script lang="ts">
   import BaseLayout from '~components/BaseLayout.svelte';
+  import Autocomplete from '~components/Autocomplete.svelte';
+
+  type Genre = {
+    value: string;
+    label: string;
+  };
+
+  const genres: Genre[] = [
+    { value: 'business', label: 'Бизнес' },
+    { value: 'detective', label: 'Детектив' },
+    { value: 'drama', label: 'Драма' },
+    { value: 'history', label: 'История' },
+    { value: 'comedy', label: 'Комедия' },
+    { value: 'novel', label: 'Роман' },
+    { value: 'fantastic', label: 'Фантастика' },
+    { value: 'fantasy ', label: 'Фэнтези' },
+  ];
+
+  const handleSelect = (e) => {
+    console.log('Value: ', e.detail.value);
+  };
 </script>
 
 <BaseLayout>
-  <p>Book page</p>
+  <Autocomplete
+    showChevron
+    items={genres}
+    placeholder="Выберите жанр"
+    onSelect={handleSelect}
+  />
 </BaseLayout>
